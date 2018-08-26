a7ee53359e4fd3f7266ed2463f80b7fc93b1734d
antonino <antomani103@gmail.com>
2018-08-26 02:01:48 +0200

    rewritten view skeleton

--- src/game_kernel/ecs/system.rs
+++ src/game_kernel/ecs/system.rs
@@ -1,5 +1,29 @@
+use super::super::settings::Config;
+
+use std::fmt;
+
+struct SysErr
+{
+
+}
+
+impl SysErr
+{
+    pub fn new() -> Self
+    {
+        Self{}
+    }
+}
+
+impl fmt::Display for SysErr
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "")
+    }
+}
+
 trait System
 {
-    fn init();
+    fn init(config: &mut Config) -> Result<(), SysErr>;
     fn run();
 }
--- src/game_kernel/settings/mod.rs
+++ src/game_kernel/settings/mod.rs
@@ -1,12 +1,27 @@
-extern crate toml;
-#[macro_use]
-extern crate serde_derive;
 use toml::macros::Deserialize;
 use toml::value::Table;
 
 use toml::*;
 
 
-trait ConfigModule{
+pub trait ConfigModule{
     fn get_name() -> &'static str;
+}
+
+pub struct Config {
+    t : Table
+}
+
+impl Config {
+    fn new<T : AsRef<str>>(x : T) -> Self {
+        if let Value::Table(t) = x.as_ref().parse().unwrap() {
+            return Config{t}
+        } else {
+            panic!("...");
+        }
+    }
+
+    fn conf_for_module<'a,T : Deserialize<'a> + ConfigModule>(&mut self) -> T {
+        self.t.remove(T::get_name()).unwrap().try_into().unwrap()
+    }
 }
